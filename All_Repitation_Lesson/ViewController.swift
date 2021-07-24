d996ef86daa5e533328b7ea66d8bae514090708c
coder7504 <asliddin2703@gmail.com>
2021-07-24 15:37:30 +0500

    Initial Commit

---
+++ All_Repitation_Lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  All_Repitation_Lesson
+//
+//  Created by Asliddin Mahmudov on 7/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
